fc79772cf5377e37521835236d382c3ff7c57984
kradragon <tuanna.hsp@gmail.com>
2016-12-19 21:35:19 +0700

    add - project template with LED and wav player

---
+++ EWARM/README.md
@@ -0,0 +1,2 @@
+Simple multi tasks demo using **FreeRTOS** on **STM32F4 Discovery Board**.
+Open project using **IAR Embedded Workbench** (on Windows).
---
+++ src/main.c
@@ -0,0 +1,156 @@
+/**
+  ******************************************************************************
+  * @file    Audio_playback_and_record/src/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    28-October-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Private typedef -----------------------------------------------------------*/
+GPIO_InitTypeDef  GPIO_InitStructure;
+/* Private define ------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+#if defined MEDIA_USB_KEY
+ USB_OTG_CORE_HANDLE          USB_OTG_Core;
+ USBH_HOST                    USB_Host;
+#endif
+
+RCC_ClocksTypeDef RCC_Clocks;
+__IO uint8_t RepeatState = 0;
+__IO uint16_t CCR_Val = 16826;
+extern __IO uint8_t LED_Toggle;
+
+/* Private function prototypes -----------------------------------------------*/
+static void TIM_LED_Config(void);
+static void PlayMusic(void *pvParameters);
+static void TaskB(void *pvParameters);
+static void TaskC(void *pvParameters);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+*/
+int main(void)
+{   
+  /* Initialize LEDS */
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);
+  STM_EVAL_LEDInit(LED5);
+  STM_EVAL_LEDInit(LED6);  
+  
+  /* SysTick end of count event each 10ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+  
+  /* GPIOD Periph clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  
+   /* Green Led On: start of application */
+  STM_EVAL_LEDOn(LED4);
+
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  
+  xTaskCreate(PlayMusic, (signed char*)"PlayMusic", 128, NULL, 1, NULL);
+  xTaskCreate(TaskB, (signed char*)"TaskB", 128, NULL, 3, NULL);
+  xTaskCreate(TaskC, (signed char*)"TaskC", 128, NULL, 2, NULL);        
+  vTaskStartScheduler();  
+}
+
+ static void TaskB(void *pvParameters)
+ { 
+     for(;;) 
+    {
+        GPIO_SetBits(GPIOD, GPIO_Pin_13);
+        vTaskDelay(50);
+        GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+        vTaskDelay(50);
+    }
+ }
+
+static void TaskC(void *pvParameters)
+ {   
+     for(;;) 
+      {
+         GPIO_SetBits(GPIOD, GPIO_Pin_14);
+         vTaskDelay(200);
+         GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+         vTaskDelay(200);
+     }
+  }
+
+static void PlayMusic(void *pvParameters)
+{    
+  /* Initialize the repeat status */
+  RepeatState = 0;
+  LED_Toggle = 7;
+  
+#if defined MEDIA_IntFLASH
+  
+  WavePlayBack(I2S_AudioFreq_48k); 
+  while (1);
+  
+#elif defined MEDIA_USB_KEY
+  
+  /* Initialize User Button */
+  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
+   
+  /* Init Host Library */
+  USBH_Init(&USB_OTG_Core, USB_OTG_FS_CORE_ID, &USB_Host, &USBH_MSC_cb, &USR_Callbacks);
+  
+  while (1)
+  {
+    /* Host Task handler */
+    USBH_Process(&USB_OTG_Core, &USB_Host);
+  }
+  
+#endif
+ }
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+  
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
