3ffc1bb40f26716bf703532ad73999e31e69ef30
kradragon <tuanna.hsp@gmail.com>
2017-01-05 21:26:05 +0700

    add - detect button press task

--- src/main.c
+++ src/main.c
@@ -1,66 +1,78 @@
-/**
-  ******************************************************************************
-  * @file    Audio_playback_and_record/src/main.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    28-October-2011
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  */ 
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
+#include "semphr.h"
 
 #include "stm32f4xx.h"
-/* Include my libraries here */
 #include "defines.h"
 #include "tm_stm32f4_delay.h"
-//#include "tm_stm32f4_disco.h"
 #include "tm_stm32f4_fatfs.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4_discovery.h"
 #include <stdio.h>
 #include <string.h>
 
 /* Private typedef -----------------------------------------------------------*/
 GPIO_InitTypeDef  GPIO_InitStructure;
-/* Private define ------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-#if defined MEDIA_USB_KEY
- USB_OTG_CORE_HANDLE          USB_OTG_Core;
- USBH_HOST                    USB_Host;
-#endif
 
+/* Private define ------------------------------------------------------------*/
+#define FROM_MILLIS(x) (x / portTICK_RATE_MS)
+/* Audio file size and start address are defined here since the audio file is 
+stored in Flash memory as a constant table of 16-bit data */
+#define AUDIO_FILE_SZE          990000
+#define AUIDO_START_ADDRESS     58 /* Offset relative to audio file header size */
+
+/* Private variables ---------------------------------------------------------*/
 RCC_ClocksTypeDef RCC_Clocks;
 __IO uint8_t RepeatState = 0;
 __IO uint16_t CCR_Val = 16826;
-extern __IO uint8_t LED_Toggle;
+xQueueHandle ledQ, musicQ;
+
+extern __IO uint8_t AudioPlayStart;
+extern uint16_t AUDIO_SAMPLE[];
+extern __IO uint8_t PauseResumeStatus ;
 
 /* Private function prototypes -----------------------------------------------*/
-static void TIM_LED_Config(void);
 static void PlayMusic(void *pvParameters);
 static void ReadSDCard(void *pvParameters);
-static void TaskB(void *pvParameters);
-static void TaskC(void *pvParameters);
+static void ControlLED3(void *pvParameters);
+static void DetectButtonPress(void *pvParameters);
+
+void initHW();
+
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
+* @brief  Main program.
+* @param  None
+* @retval None
 */
 int main(void)
 {   
+  initHW();
+  
+  /* Create IPC variables */
+  ledQ = xQueueCreate(10, sizeof(int));
+  musicQ = xQueueCreate(10, sizeof(int));  
+  if (ledQ == 0 || musicQ == 0) {
+    while(1); /* fatal error */
+  }
+  
+  xTaskCreate(PlayMusic, (signed char*)"PlayMusic", 1024, NULL, 1, NULL);
+  xTaskCreate(ControlLED3, (signed char*)"ControlLED3", 128, NULL, 4, NULL);    
+  xTaskCreate(DetectButtonPress, (signed char*)"DetectButtonPress", 128, NULL, 1, NULL);
+  xTaskCreate(ReadSDCard, (signed char*)"ReadSDCard", 4096, NULL, 2, NULL);
+  vTaskStartScheduler();  
+  
+  while (1);
+}
+
+void initHW()
+{  
   /* Initialize LEDS */
   STM_EVAL_LEDInit(LED3);
   STM_EVAL_LEDInit(LED4);
@@ -71,127 +83,163 @@ int main(void)
   RCC_GetClocksFreq(&RCC_Clocks);
   SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
   
-   /* Green Led On: start of application */
-  //STM_EVAL_LEDOn(LED4);
-  
-  /* GPIOD Periph clo
-  ck enable 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);*/
-  
-  xTaskCreate(PlayMusic, (signed char*)"PlayMusic", 128, NULL, 1, NULL);
-  xTaskCreate(TaskB, (signed char*)"TaskB", 128, NULL, 4, NULL);
-  xTaskCreate(TaskC, (signed char*)"TaskC", 128, NULL, 3, NULL);        
-  xTaskCreate(ReadSDCard, (signed char*)"ReadSDCard", 1024, NULL, 2, NULL);
-  vTaskStartScheduler();  
+  // Init PushButton
+  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_GPIO);
 }
 
- static void ReadSDCard(void *pvParameters){
-    FATFS FatFs;
-    FIL fil;
-    uint32_t free, total;
-    
-    STM_EVAL_LEDOn(LED5);
-    while (f_mount(&FatFs, "0:", 1) != FR_OK) {
-      vTaskDelay(1000);
-    }
-    STM_EVAL_LEDOff(LED5);    
-    STM_EVAL_LEDOn(LED4);
-    
-    while (f_open(&fil, "0:1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) != FR_OK) {
-      vTaskDelay(1000);
-    }
-    
-    TCHAR line[100];
-    while (1) {
-      TCHAR* result = f_gets(line, 100, &fil);
-      if (result != NULL) {    
-        printf("%s", line);
-        STM_EVAL_LEDToggle(LED4);
+static void DetectButtonPress(void *pvParameters) {
+  int sig = 1, ss = 1;
+  while (1) {
+    /* Detect Button Press  */
+    if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0) > 0) {
+      while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0) > 0)
+        vTaskDelay(100 / portTICK_RATE_MS);
+      
+      if (ss == 1) {
+        xQueueSendToBack(ledQ, &sig, 0); 
       }
-      else { 
-        printf("EOF");
-        break;
+      if (ss == 0) {
+        xQueueSendToBack(musicQ, &sig, 0); 
       }
-      vTaskDelay(1000);
-    }   
-    f_close(&fil);
-    f_mount(0, "0:", 1);
-    
-    STM_EVAL_LEDOff(LED4);
-    while(1) {        
-      STM_EVAL_LEDToggle(LED5);
-      vTaskDelay(300);
+      ss = !ss;
     }
- }
-
- static void TaskB(void *pvParameters)
- { 
-     for(;;) 
-    {
-        STM_EVAL_LEDToggle(LED6);
-        vTaskDelay(500);
-    }
- }
-
-static void TaskC(void *pvParameters)
- {   
-     for(;;) 
-      {
-         STM_EVAL_LEDToggle(LED3);
-         vTaskDelay(200);
-     }
   }
+}
+
+static void ReadSDCard(void *pvParameters) {
+  FATFS FatFs;
+  FIL fil;
+  
+  // Turn on RED led then wait for SD card to be ready
+  STM_EVAL_LEDOn(LED5);
+  printf("Waiting for SD card...\n");
+  while (f_mount(&FatFs, "0:", 1) != FR_OK) {
+    vTaskDelay(FROM_MILLIS(1000));
+  }
+  
+  // Open file and keep toggle RED led if error occurs
+  printf("Opening file...\n");
+  while (f_open(&fil, "0:1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) != FR_OK) {
+    vTaskDelay(FROM_MILLIS(500));     
+    STM_EVAL_LEDToggle(LED5);
+  }
+  
+  STM_EVAL_LEDOff(LED5);
+  STM_EVAL_LEDOn(LED4);
+  printf("Reading...\n");
+  TCHAR line[100];
+  while (1) {
+    TCHAR* result = f_gets(line, 100, &fil);
+    if (result != NULL) {    
+      printf("%s", line);
+      STM_EVAL_LEDToggle(LED4);
+    }
+    else { 
+      printf("EOF");
+      break;
+    }
+    vTaskDelay(FROM_MILLIS(1000));
+  }   
+  f_close(&fil);
+  f_mount(0, "0:", 1);
+  
+  STM_EVAL_LEDOff(LED4);
+  while(1) {        
+    STM_EVAL_LEDToggle(LED5);
+    vTaskDelay(FROM_MILLIS(300));
+  }
+}
+
+static void ControlLED3(void *pvParameters)
+{ 
+  int sig = 1, state = 1;
+  portBASE_TYPE status;  
+  
+  for(;;) 
+  {    
+    status = xQueueReceive(ledQ, &sig, 0);
+    if (status == pdTRUE) {
+      state = !state;
+    }
+    
+    if (state) {
+      STM_EVAL_LEDToggle(LED3);
+    }
+    else {
+      STM_EVAL_LEDOn(LED3);
+    }
+    vTaskDelay(FROM_MILLIS(500));
+  }
+}
 
 static void PlayMusic(void *pvParameters)
 {    
-  /* Initialize the repeat status */
-  RepeatState = 0;
-  LED_Toggle = 7;
+  /* Start playing */
+  AudioPlayStart = 1;
+  RepeatState =0;
+  int sig = 1, isPausing = 0;
+  portBASE_TYPE status;  
   
-#if defined MEDIA_IntFLASH
+  /* Initialize wave player (Codec, DMA, I2C) */
+  WavePlayerInit(I2S_AudioFreq_48k);
   
-  WavePlayBack(I2S_AudioFreq_48k); 
-  while (1);
+  /* Play on */
+  AudioFlashPlay((uint16_t*)(AUDIO_SAMPLE + AUIDO_START_ADDRESS),AUDIO_FILE_SZE,AUIDO_START_ADDRESS);
   
-#elif defined MEDIA_USB_KEY
-  
-  /* Initialize User Button */
-  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
-   
-  /* Init Host Library */
-  USBH_Init(&USB_OTG_Core, USB_OTG_FS_CORE_ID, &USB_Host, &USBH_MSC_cb, &USR_Callbacks);
-  
-  while (1)
-  {
-    /* Host Task handler */
-    USBH_Process(&USB_OTG_Core, &USB_Host);
+  /* Infinite loop */
+  while(1)
+  { 
+    /* check on the repeate status */
+    if (RepeatState == 0)
+    {      
+      status = xQueueReceive(musicQ, &sig, 0);
+      if (status == pdTRUE) {
+        PauseResumeStatus = isPausing ? 1 : 0;
+        isPausing = !isPausing;
+      }
+      
+      if (PauseResumeStatus == 0)
+      {
+        /* Pause playing */
+        WavePlayerPauseResume(PauseResumeStatus);
+        PauseResumeStatus = 2;
+        STM_EVAL_LEDOn(LED6);
+      }
+      else if (PauseResumeStatus == 1)
+      {
+        /* Resume playing */
+        WavePlayerPauseResume(PauseResumeStatus);
+        PauseResumeStatus = 2;
+      }
+      else
+      {        
+        STM_EVAL_LEDToggle(LED6);
+      }
+    }
+    else
+    {
+      /* Stop playing */
+      WavePlayerStop();
+    }
+    
+    vTaskDelay(FROM_MILLIS(200));
   }
-  
-#endif
- }
+}
 
 #ifdef  USE_FULL_ASSERT
 
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *   where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+* @brief  Reports the name of the source file and the source line number
+*   where the assert_param error has occurred.
+* @param  file: pointer to the source file name
+* @param  line: assert_param error line source number
+* @retval None
+*/
 void assert_failed(uint8_t* file, uint32_t line)
 { 
   /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
+  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  
   /* Infinite loop */
   while (1)
   {
@@ -199,5 +247,5 @@ void assert_failed(uint8_t* file, uint32_t line)
 }
 #endif
 
-  
+
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
